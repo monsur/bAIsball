dab0c2264804d144afeae65a34695e5f75d3c828
Monsur Hossain <monsur@gmail.com>
2025-05-03 21:53:30 -0500

    Add an S3 uploader

---
+++ podcaster/src/upload.py
@@ -0,0 +1,29 @@
+import boto3
+import os
+from podcaster.src import args_helper
+from podcaster.src import logger_helper
+
+logger = logger_helper.get_logger(__name__)
+
+def run(args):
+    s3 = boto3.client('s3')
+
+    for root, dirs, files in os.walk(args.output_dir):
+        for filename in files:
+            # construct the full local path
+            source_path = os.path.join(root, filename)
+            
+            # construct the full upload paths
+            relative_path = os.path.relpath(source_path, args.output_dir)
+            s3_path = os.path.join(args.date, relative_path)
+            
+            extra_args = {}
+            # If its an MP3, make it publicly accessible
+            if (source_path.endswith(".mp3")):
+                extra_args["ACL"] = "public-read"
+
+            logger.info("Uploading %s..." % s3_path)
+            s3.upload_file(source_path, args.s3_bucket, s3_path, ExtraArgs=extra_args)
+
+if __name__ == "__main__":
+    run(args_helper.get_args())
