75b4de0a7471317f49bc37bd775c30640313010b
Monsur Hossain <monsur@gmail.com>
2025-05-04 16:16:56 -0500

    Walk through the whole flow in main.py

--- podcaster/src/main.py
+++ podcaster/src/main.py
@@ -1,5 +1,13 @@
+from podcaster.src import args_helper, archive, audio, data, prompt, rss, transcript
+
 def main():
-    pass
+    args = args_helper.get_args()
+    data.run(args)
+    prompt.run(args)
+    transcript.run(args)
+    audio.run(args)
+    rss.run(args)
+    archive.run(args)
 
 if __name__ == "__main__":
     main() 
