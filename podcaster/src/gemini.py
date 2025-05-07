94ac24bf6c6d1c20c3b2cc3ac2f9cab4ecc77e77
Monsur Hossain <monsur@gmail.com>
2025-05-07 07:55:16 -0500

    Move temperature into a variable

--- podcaster/src/gemini.py
+++ podcaster/src/gemini.py
@@ -6,6 +6,7 @@ class Gemini:
     def __init__(self):
         self.client = genai.Client(api_key=os_helper.getenv('GEMINI_API_KEY'))
         self.model = "gemini-2.5-pro-exp-03-25"
+        self.temperature = 0.2
 
     def get_response(self, prompt, system_instructions):
         try:
@@ -14,7 +15,7 @@ class Gemini:
                 contents=prompt,
                 config=types.GenerateContentConfig(
                     system_instruction=[system_instructions],
-                    temperature=0.2
+                    temperature=self.temperature
                 ),
             )
             return response.text
